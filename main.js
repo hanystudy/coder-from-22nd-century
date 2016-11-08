60e1c9aeec79f7f93bdd0dd95c681a8b3ba468cb
Yi Han <hanystudy@gmail.com>
2016-11-08 17:44:59 +0800

    start server by default

--- main.js
+++ main.js
@@ -56,5 +56,6 @@ mainApp.on('activate', function () {
   }
 })
 
+require('./server/app')
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
